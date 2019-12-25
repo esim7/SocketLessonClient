3aa24056b3f9a9d2ec52f3bb099c452b3ff38b15
makishev@yahoo.com <makishev@yahoo.com>
2019-12-25 21:42:13 +0600

    client

---
+++ ClientSocket/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientSocket
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                socket.Connect(endPoint);
+
+                var buffer = Encoding.UTF8.GetBytes(textBox_text.Text);
+
+
+                socket.Send(buffer);
+                socket.Shutdown(SocketShutdown.Both);
+            }
+        }
+    }
+}
